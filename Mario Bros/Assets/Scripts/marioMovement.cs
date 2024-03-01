bc8090170835b3fd4f4db392b460ee231741c199
Jamesliangk9 <104614862+Jamesliangk9@users.noreply.github.com>
2024-03-01 16:24:08 +1100

    Fixed the problem
    fixed the problem where mario could stick to the wall while jumping

--- Mario Bros/Assets/Scripts/marioMovement.cs
+++ Mario Bros/Assets/Scripts/marioMovement.cs
@@ -7,23 +7,25 @@ public class MarioController : MonoBehaviour
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;
-    private bool isGrounded = true;
+    private bool isGrounded = false;
     private bool facingRight = true;
+    public PhysicsMaterial2D friction; 
+    public PhysicsMaterial2D noFriction;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        GetComponent<Rigidbody2D>().sharedMaterial = friction;
     }
 
     private void Update()
     {
         // Check if Mario is grounded using Rigidbody2D's IsTouchingLayers method
         isGrounded = rb.IsTouchingLayers(groundLayer);
-
-        // Check for jumping input in the Update method
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            GetComponent<Rigidbody2D>().sharedMaterial = noFriction;
         }
     }
 
